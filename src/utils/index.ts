714712120132b745a8e817546bd9789d3b84a29a
王耀辉 <wangyaohui@wangxiaohuideMacBook-Pro.local>
2021-06-27 13:14:50 +0800

    useMount和useDebounce两个hook

---
+++ src/utils/index.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: number) => (value === 0 ? false : !value);
+
+// 在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object: object) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    // @ts-ignore
+    const value = result[key];
+    if (isFalsy(value)) {
+      // @ts-ignore
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export const useDebounce = (value: object, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
+};
